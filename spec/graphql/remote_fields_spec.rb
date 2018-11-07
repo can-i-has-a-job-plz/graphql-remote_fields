15a345bd9eda0c389a0f0926f4023b4e56a228cc
ojab <ojab@ojab.ru>
2018-11-07 11:18:18 +0300

    Fixup spec

--- spec/graphql/remote_fields_spec.rb
+++ spec/graphql/remote_fields_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe GraphQL::RemoteFields do
   it "has a version number" do
     expect(GraphQL::RemoteFields::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
